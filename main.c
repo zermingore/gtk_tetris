0fea0b9f2ec622883042e2a6d097839e4eb00399
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-05-24 18:47:21 +0200

    [Graphics] Drawing a rectangle instead of a triangle

--- main.c
+++ main.c
@@ -80,9 +80,15 @@ static gboolean draw()
   glDeleteShader(fragmentShader);
 
   float vertices[] = {
-    -0.5f, -0.5f, 0.0f, // left
-    0.5f, -0.5f, 0.0f,  // right
-    0.0f,  0.5f, 0.0f   // top
+    -0.5f, -0.5f, 0.0f, // bottom left
+    0.5f,  -0.5f, 0.0f, // bottom right
+    0.5f,  0.5f,  0.0f, // top right
+    -0.5f, 0.5f,  0.0f  // top left
+  };
+
+  unsigned int idx[] = {
+    0, 1, 3,
+    1, 2, 3
   };
 
   unsigned int vbo;
@@ -95,6 +101,13 @@ static gboolean draw()
   glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  unsigned int ebo;
+  glGenBuffers(1, &ebo);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idx), idx, GL_STATIC_DRAW);
+
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
   glEnableVertexAttribArray(0);
 
@@ -105,10 +118,12 @@ static gboolean draw()
   // loop
   glClearColor(.95, .95, .95, 0);
   glClear(GL_COLOR_BUFFER_BIT);
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   glUseProgram(shaderProgram);
   glBindVertexArray(vao);
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  glBindVertexArray(0);
 
 
   return TRUE;

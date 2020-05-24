ede5c070a8a294ef814cb618ab4388adec9c815c
Jean-Philippe Quenord <jp.quenord+git@gmail.com>
2020-05-24 22:30:50 +0200

    [Graphics] Cleaner shader (using swizzling)

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ static int init_render_window()
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
     "}\0";
 
   const char *fragmentShaderSource =
